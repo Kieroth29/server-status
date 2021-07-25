eaba07ab6a9c1543596e93392e39311442fde5b6
Marcos Vinícius Queiroz <49909597+Kieroth29@users.noreply.github.com>
2021-07-25 13:54:59 -0300

    Update __init__.py

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -26,4 +26,7 @@ def create_app(test_config=None):
     app.register_blueprint(main.bp)
     app.add_url_rule('/', endpoint="index")
 
+    if __name__ == "__main__":
+        app.run(host='0.0.0.0')
+
     return app
