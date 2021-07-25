2924c2625d9e877a6bd2f4c48a06edc0d23ee8ed
root <kkinhuxd@gmail.com>
2021-07-25 18:15:57 -0300

    Modoficando WSGI

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-    r = requests.get("https://api.mcsrvstat.us/2/kieroth29.xyz")
-    r = r.json()
+   r = requests.get("https://api.mcsrvstat.us/2/kieroth29.xyz")
+   r = r.json()
 
-    return render_template("main.html", r=r)
+   return render_template("main.html", r=r)
 
 if __name__ == '__main__':
     app.run('0.0.0.0')
